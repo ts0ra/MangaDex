01130a4ad6c21db972b09f98c6245b37dc0d2e55
ts0ra <gaming.heroes354@gmail.com>
2025-05-24 15:22:21 +0700

    feat: ketinggalan

---
+++ core/src/main/java/com/ts0ra/core/data/source/remote/response/TagsItem.kt
@@ -0,0 +1,18 @@
+package com.ts0ra.core.data.source.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class TagsItem(
+
+	@field:SerializedName("relationships")
+	val relationships: List<RelationshipsItem?>? = null,
+
+	@field:SerializedName("attributes")
+	val attributes: Attributes? = null,
+
+	@field:SerializedName("id")
+	val id: String? = null,
+
+	@field:SerializedName("type")
+	val type: String? = null
+)
