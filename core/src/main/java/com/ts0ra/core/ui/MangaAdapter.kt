e6c060d51acbf8f3fdd67ae2f53684b31978125f
ts0ra <gaming.heroes354@gmail.com>
2025-05-24 15:21:48 +0700

    feat: added manga adapter

---
+++ core/src/main/java/com/ts0ra/core/ui/MangaAdapter.kt
@@ -0,0 +1,58 @@
+package com.ts0ra.core.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ts0ra.core.databinding.ItemListMangaBinding
+import com.ts0ra.core.domain.model.Manga
+
+class MangaAdapter : ListAdapter<Manga, MangaAdapter.ListViewHolder>(DIFF_CALLBACK) {
+
+    var onItemClick: ((Manga) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder =
+        ListViewHolder(
+            ItemListMangaBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val data = getItem(position)
+        holder.bind(data)
+    }
+
+    inner class ListViewHolder(private var binding: ItemListMangaBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Manga) {
+            Glide.with(itemView.context)
+                .load("https://uploads.mangadex.org/covers/${data.mangaId}/${data.coverArtFile}")
+                .into(binding.imageCoverArt)
+            binding.textTitle.text = data.title
+        }
+
+        init {
+            itemView.setOnClickListener {
+                onItemClick?.invoke(getItem(bindingAdapterPosition))
+            }
+        }
+    }
+
+    companion object {
+        val DIFF_CALLBACK: DiffUtil.ItemCallback<Manga> =
+            object : DiffUtil.ItemCallback<Manga>() {
+                override fun areItemsTheSame(oldItem: Manga, newItem: Manga): Boolean {
+                    return oldItem.mangaId == newItem.mangaId
+                }
+
+                override fun areContentsTheSame(oldItem: Manga, newItem: Manga): Boolean {
+                    return oldItem == newItem
+                }
+            }
+    }
+}
