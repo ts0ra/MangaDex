137ff52e319764dab982b02276877ada7ffcd6f4
ts0ra <gaming.heroes354@gmail.com>
2025-05-24 15:21:01 +0700

    feat: added koin and glide dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -41,9 +41,10 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    api(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    api(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -58,4 +59,12 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.logging.interceptor)
+
+    // Koin
+    api(libs.koin.android)
+
+    // Glide
+    api(libs.glide)
+    ksp(libs.ksp)
+
 }
