91facecbb629e105d20d2a594c00df76bca0b4f4
ts0ra <gaming.heroes354@gmail.com>
2025-05-28 00:04:12 +0700

    feat: delete unused directive

--- app/src/main/java/com/ts0ra/mangadex/MainActivity.kt
+++ app/src/main/java/com/ts0ra/mangadex/MainActivity.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
